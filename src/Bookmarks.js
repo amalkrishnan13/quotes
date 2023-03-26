2abcd8c392048bb187133d7cc782be9f2b9e21ba
unknown <amalkrishnan512@gmail.com>
2023-03-26 13:04:47 +0530

    initial

---
+++ src/Bookmarks.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Bookmarks = () => {
+  return (
+    <div>
+        book
+      
+    </div>
+  )
+}
+
+export default Bookmarks
---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+// import Container from 'react-bootstrap/Container';
+// import Navbar from 'react-bootstrap/Navbar';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import "./Header.css"
+
+function Header() {
+  return (
+    <div className='head'>
+        {/* <Container>
+        
+      <h4 className='head'>Home</h4>
+      </Container>
+      <h4 className='head2' >Bookmarks</h4> */}
+
+    
+      
+    </div>
+  )
+}
+
+export default Header
